bc1ec177420a0b7576a87bd5394aa184d2548b7d
Chuck Michael <chuckqs@yahoo.com>
2017-12-07 22:25:08 -0500

    Basic React Layout

--- react-client/src/components/App.js
+++ react-client/src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HeaderContainer from "./containers/HeaderContainer";
-import HomeContainer from "./containers/HomeContainer";
-import SingleRecipeContainer from "./containers/SingleRecipeContainer";
-import SearchContainer from "./containers/SearchContainer";
+import HeaderContainer from "../containers/HeaderContainer";
+import HomeContainer from "../containers/HomeContainer";
+import SingleRecipeContainer from "../containers/SingleRecipeContainer";
+import SearchContainer from "../containers/SearchContainer";
 
 class App extends Component {
   render() {
