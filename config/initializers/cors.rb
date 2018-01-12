c7122afd9128b50149a731bbd545995a8d98acb5
Yi-Xuan Lau <yixuan.lau@gmail.com>
2018-01-12 22:12:44 +0800

    -Update cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://gentle-dawn-10801.herokuapp.com', 'http://recipeshoppingapp.com', 'http://gentle-dawn-10801.herokuapp.com', 'https://recipeshoppingapp.com'
 
     resource '*',
       headers: :any,
